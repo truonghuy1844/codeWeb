31fc9f5ac2e959222b72d95acf6f644a4d4e245f
Vu Phuong <pvu07082004@gmail.com>
2025-05-26 21:38:56 +0700

    Thêm file backend và frontend

---
+++ ToyStore- Menu/Models/CartItemDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ToyStore.Models
+{
+    public class CartItemDto
+    {
+        public int UserId { get; set; }
+        public string ProductId { get; set; } = string.Empty;
+        public string ProductName { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
