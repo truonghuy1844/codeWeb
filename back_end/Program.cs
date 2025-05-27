9d5d0c72a22f3fe9d7d30341d65804b2ec00ecaa
Trương Quốc Huy <huytruong0935@gmail.com>
2025-05-27 21:09:02 +0700

    fix

--- back_end/Program.cs
+++ back_end/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddDbContext<AppDbContext>(options =>
+builder.Services.AddDbContext<WebCodeContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
--- back_end/obj/Debug/net9.0/back_end.AssemblyInfo.cs
+++ back_end/obj/Debug/net9.0/back_end.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back_end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4092d3a82f0edf41017c1f1a9597292647112a0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b9ab3a7fea13c7b5709f8e227a7a556af1480d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("back_end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back_end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
