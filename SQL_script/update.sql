a9378f82b5f57ad66fc18563b4a1bd671f814fba
Trương Quốc Huy <huytruong0935@gmail.com>
2025-05-29 09:25:40 +0700

    thêm phòng ban

--- SQL_script/update.sql
+++ SQL_script/update.sql
@@ -1,3 +1,5 @@
+USE web_code
+GO
 ALTER TABLE address
 ALTER COLUMN user_id INT;
 go
@@ -6,3 +8,5 @@ ADD CONSTRAINT FK_address_user
 FOREIGN KEY (user_id)
 REFERENCES user_tb(user_id);
 
+ALTER TABLE user_tb2
+ADD phong_ban nvarchar(50);
