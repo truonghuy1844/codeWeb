030080cf98d4945ab74f68c4dc000879e91b7c9d
Lander Nguyen <work.beros@gmail.com>
2025-06-03 14:28:52 +0700

    fix: fix bug create user in user management

--- back_end/obj/Debug/net9.0/back_end.AssemblyInfo.cs
+++ back_end/obj/Debug/net9.0/back_end.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back_end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1a66d11486800135f02bb62d677770685557e5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d18c8ac5f1e6f20626d520611bd2696b8a8a807f")]
 [assembly: System.Reflection.AssemblyProductAttribute("back_end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back_end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
